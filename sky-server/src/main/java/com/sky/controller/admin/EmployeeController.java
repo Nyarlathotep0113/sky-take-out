b8cd37299726fb97c482b6a912c6d757df524422
z <1277177147@qq.com>
2025-09-18 19:52:02 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -8,6 +9,8 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +27,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/admin/employee")
 @Slf4j
+@Api(tags = "员工相关接口")
 public class EmployeeController {
 
     @Autowired
@@ -37,6 +41,7 @@ public class EmployeeController {
      * @param employeeLoginDTO
      * @return
      */
+    @ApiOperation("员工登录")
     @PostMapping("/login")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
         log.info("员工登录：{}", employeeLoginDTO);
@@ -66,9 +71,31 @@ public class EmployeeController {
      *
      * @return
      */
+    @ApiOperation("员工退出")
     @PostMapping("/logout")
     public Result<String> logout() {
         return Result.success();
     }
 
+    /**
+     * 新增员工接口
+     * 用于保存新员工的信息
+     *
+     * @param employeeDTO 员工信息DTO，包含需要保存的员工数据
+     * @return 返回操作结果
+     */
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        /**
+         * 完成用户注册有两种思路：
+         * 思路1：先访问数据库检查是否存在对应的主键，再进行注册
+         * 适用于注册需要实时校验，需要立即反馈的场景，但存在并发安全的问题
+         * 思路2：直接插入，如果主键冲突则抛出异常
+         * 适用于高并发场景，但需要捕获异常进行处理
+         */
+        log.info("新增员工：{}", employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -23,5 +26,21 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
     }
-
+    /**
+     * 数据库重复键异常处理器
+     *
+     * @param ex SQL完整性约束违规异常
+     * @return Result 包含错误信息的 Result 对象
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message=ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String duplicatedUsername=message.split(" ")[2];
+            String msg=duplicatedUsername+MessageConstant.ALREADY_EXIST;
+            return Result.error(msg);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
