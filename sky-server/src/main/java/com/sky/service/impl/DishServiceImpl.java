20e8841201a39dbfdd82d3ecf4bc7b0a78829dc8
z <1277177147@qq.com>
2025-09-25 16:03:13 +0800

    菜品分页查询业务功能开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,16 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.FlavorMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +47,15 @@ public class DishServiceImpl implements DishService {
             flavorMapper.insertBatch(flavors);
         }
 
+    }
+
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+        Page<DishVO> dishPage=dishMapper.pageQuery(dishPageQueryDTO);
+        long total=dishPage.getTotal();
+        List<DishVO> records=dishPage.getResult();
+        PageResult pageResult=new PageResult(total,records);
+        return pageResult;
     }
 }
